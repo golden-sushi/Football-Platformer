caa2cb4348d3fdc460e7ac5dd4ac4251d5bb4eaf
Karim Smires <k.smires1686@gmail.com>
2025-04-28 20:59:21 -0400

    fixed mouse bug
    mouse properly locks between main menu and regular view

---
+++ FootballGame/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // lock cursor to center of game window when in game scene
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // handle loss of focus
+    void OnApplicationFocus(bool hasFocus)
+    {
+        if (hasFocus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
